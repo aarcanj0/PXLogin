8b0b43b2ca4f229b8bb5e001b5c6cd51a5a878c2
aarcanj0 <66854515+aarcanj0@users.noreply.github.com>
2022-11-03 13:27:49 -0300

    Add files via upload

---
+++ src/javaapplication1/Program.java
@@ -0,0 +1,20 @@
+
+
+
+
+package javaapplication1;
+
+/**
+ *
+ * @author guiar
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new loginWindow().setVisible(true);
+    }
+    
+}
